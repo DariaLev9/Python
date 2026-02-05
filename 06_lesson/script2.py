5b63dbeb9d2f459a4897564898f2ec4c9c5a79ec
admin <admin@MacBook-Pro-Dara.local>
2026-02-05 13:55:16 +0300

    Lesson 6 homework

---
+++ 06_lesson/script2.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.implicitly_wait(20)
+
+driver.get("http://uitestingplayground.com/textinput")
+
+driver.find_element(By.CSS_SELECTOR, "#newButtonName").send_keys("SkyPro")
+
+driver.find_element(By.CSS_SELECTOR, "#updatingButton").click()
+
+btn_text = driver.find_element(By.CSS_SELECTOR, "#updatingButton").text
+print(btn_text)
+
+driver.quit()
